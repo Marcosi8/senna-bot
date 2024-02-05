1331a1b95dc46d6ddc003d2205b4a5aae39f2671
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-05 20:24:39 -0300

    Update main-speedtest.js

--- plugins/main-speedtest.js
+++ plugins/main-speedtest.js
@@ -11,7 +11,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
         o = e
     } finally {
         let { stdout, stderr } = o
-        if (stdout.trim()) conn.loadingMsg(m.chat, 'Cargando', `*≡ SPEEDTEST.NET*\n\n${stdout}`, ['↷', '↻', '⟳', '↷', '↻', '⟳'], m) //m.reply(`*≡ SPEEDTEST.NET*\n\n${stdout}`) 
+        if (stdout.trim()) conn.loadingMsg(m.chat, 'Carregando', `*≡ SPEEDTEST.NET*\n\n${stdout}`, ['↷', '↻', '⟳', '↷', '↻', '⟳'], m) //m.reply(`*≡ SPEEDTEST.NET*\n\n${stdout}`) 
         if (stderr.trim()) m.reply(stderr)
         m.react(done) 
     }
