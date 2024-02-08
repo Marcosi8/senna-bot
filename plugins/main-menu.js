02d2f172bf9b6e7284faf9c211a1f04a8409f0bb
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-08 18:50:09 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -46,7 +46,7 @@ const defaultMenu = {
   header: '┌─⊷ *%category*',
   body: '⤿ %cmd %isdiamond %isPremium',
   footer: '└───────────\n',
-  after: `test'
+  after: `test',
 `,
 }
 let handler = async (m, { conn, usedPrefix: _p, __dirname }) => {
