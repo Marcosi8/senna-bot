274a951211a74a2cf5cf0dfc3f68954e5a81f0b1
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-08 20:43:48 -0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -16,7 +16,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
         }
         await conn.reply(m.chat, `*≡ Link Detectado*
             
-Não permitimos links desconhecidos em nosso grupo/We do not allow unknown links in our group, *@${m.sender.split('@')[0]}* O banimento é automático, contate um administrador se acha que foi um erro. ${isBotAdmin ? '' : '\n\nEu não sou um administrador, então eu não posso expulsá-lo :"v'}`, null, { mentions: [m.sender] } )
+Não permitimos links desconhecidos em nosso grupo / We do not allow unknown links in our group. *@${m.sender.split('@')[0]}* O banimento é automático, contate um administrador se acha que foi um erro. ${isBotAdmin ? '' : '\n\nEu não sou um administrador, então eu não posso expulsá-lo :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
         	await conn.sendMessage(m.chat, { delete: m.key })
             await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')
