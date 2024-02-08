31df466ea5b7aefa5bc8a8a1ea002774b661ccf1
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-08 16:51:32 -0300

    Update fun-tomp3.js

--- plugins/fun-tomp3.js
+++ plugins/fun-tomp3.js
@@ -6,9 +6,9 @@ let handler = async (m, { conn, usedPrefix, command }) => {
    let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
    // if (!/video|audio/.test(mime)) throw `✳️ Responda al video o nota de voz que desea convertir a mp3 con el comando :\n\n*${usedPrefix + command}*`
     let media = await q.download?.()
-    if (!media) throw '❎ Error al descargar medios'
+    if (!media) throw '❎ Error ao baixar'
     let audio = await toAudio(media, 'mp4')
-    if (!audio.data) throw '❎ Error al convertir'
+    if (!audio.data) throw '❎ Error ao converter'
     conn.sendFile(m.chat, audio.data, 'audio.mp3', '', m, null, { mimetype: 'audio/mp4' })
     } catch (e) {
         m.reply(`✳️ ${mssg.toaud}:\n\n*${usedPrefix + command}*`)
@@ -16,6 +16,6 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 }
 handler.help = ['tomp3']
 handler.tags = ['fun']
-handler.command = ['tomp3', 'mp3', 'toudio'] 
+handler.command = ['tomp3', 'mp3', 'audio'] 
 
 export default handler
