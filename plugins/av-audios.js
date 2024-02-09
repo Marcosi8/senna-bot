0a7ce0d271146b8dda36f082ab903d52d59c719f
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-09 11:35:21 -0300

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -17,7 +17,7 @@ let audioMsg = {
   'fino senhores': './src/mp3/fino.mp3',
   'bom dia': 'https://b.top4top.io/m_2961oeau70.mp3',
   'bot': 'https://d.top4top.io/m_2961bgor02.mp3',
-  'fake': 'https://f.top4top.io/m_2961e8ou64.mp3',
+  'fake', 'mentira', '/fake' : 'https://f.top4top.io/m_2961e8ou64.mp3',
   'paulo': 'https://c.top4top.io/m_29613e80q1.mp3',
   '@558881647724': 'https://e.top4top.io/m_29619bg673.mp3'
 }
