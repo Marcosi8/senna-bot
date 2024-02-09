f26f1747e17f983c4cd855f1f8bb4f4443b0b7a5
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-08 21:11:02 -0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -14,7 +14,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin }) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`
             if (m.text.includes(linkThisGroup)) return !0
         }
-        await conn.reply(m.chat, `*≡ Link Detectado*
+        await conn.reply(m.chat, `*≡ [❗️] Link Detectado/Link Detected*
             
 Não permitimos links desconhecidos em nosso grupo / We do not allow unknown links in our group. *@${m.sender.split('@')[0]}* O banimento é automático, contate um administrador se acha que foi um erro. ${isBotAdmin ? '' : '\n\nEu não sou um administrador, então eu não posso expulsá-lo :"v'}`, null, { mentions: [m.sender] } )
         if (isBotAdmin && chat.antiLink) {
