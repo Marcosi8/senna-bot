df07c3e371d696197fe8ab44e33b5e96beb53337
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-10 18:11:15 -0300

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,48 +1,69 @@
-const ytdl = require('ytdl-core');
 
-let limit = 320;
+import yts from 'yt-search'
+import fg from 'ytdl-core'
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
+let limit = 320
+let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
+  
+    if (!text) throw `🤔 Você quer que música? ${mssg.example} *${usedPrefix + command}* queen, don't stop me now`
+  let chat = global.db.data.chats[m.chat]
+  let res = await yts(text)
+  //let vid = res.all.find(video => video.seconds < 3600)
+  let vid = res.videos[0]
+  if (!vid) throw `❗️ Vídeo/Audio não encontrado`
+  let isVideo = /vid$/.test(command)
+  m.react('🎧') 
+  
+  let play = `
+	≡ *YT MUSIC*
+┌──────────────
+▢ 📥 *${mssg.title}:* ${vid.title}
+▢ 📆 *${mssg.aploud}:* ${vid.ago}
+▢ ⏱️ *${mssg.duration}:* ${vid.timestamp}
+▢ 👀 *${mssg.views}:* ${vid.views.toLocaleString()}
+└──────────────
 
-module.exports = {
-  cmdName: () => ({
-    name: ['play', 'musica', 'song'],
-    alias: [],
-    react: "🎧",
-    need: "text",
-    category: "dl",
-    desc: "Search and play music from YouTube.",
-  }),
-  getCommand: async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-    if (!text) throw `🤔 Do you want music? ${mssg.example} *${usedPrefix + command}* queen, don't stop me now`;
+_Enviando..._` 
+conn.sendFile(m.chat, vid.thumbnail, 'play', play, m, null, rcanal)
+  
+  let q = isVideo ? '360p' : '128kbps' 
+try {
+  let yt = await (isVideo ? fg.ytv : fg.yta)(vid.url, q)
+  let { title, dl_url, quality, size, sizeB } = yt
+  let isLimit = limit * 1024 < sizeB 
 
-    let chat = global.db.data.chats[m.chat];
-    let info = await ytdl.getInfo(text);
-    let vid = info.videoDetails;
-    if (!vid) throw `❗️ Video/Audio not found`;
-    let isVideo = /vid$/.test(command);
+     await conn.loadingMsg(m.chat, '📥 BAIXANDO', ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ COMPLETO!' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
+     
+	  if(!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /vid$/.test(command)), `
+▢ *🎞️Qualidade* : ${quality}
+▢ *⚖️Peso* : ${size}
+`.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
+		m.react(done) 
+  } catch {
+  try {
+//  let q = isVideo ? '360p' : '128kbps' 
+  let yt = await (isVideo ? fg.ytmp4 : fg.ytmp3)(vid.url, q)
+  let { title, dl_url, quality, size, sizeB } = yt
+  let isLimit = limit * 1024 < sizeB 
 
-    conn.sendFile(m.chat, vid.thumbnail.thumbnails[0].url, 'play', `
-      ≡ *YT MUSIC*
-      ┌──────────────
-      ▢ 📥 *Title:* ${vid.title}
-      ▢ 📆 *Uploaded:* ${vid.publishDate}
-      ▢ ⏱️ *Duration:* ${vid.lengthSeconds}
-      ▢ 👀 *Views:* ${vid.viewCount}
-      └──────────────
-
-      _Sending..._
-    `, m, null, rcanal);
-
-    let q = isVideo ? 'highest' : 'highestaudio';
-
-    try {
-      let stream = ytdl(text, { quality: q });
-      conn.sendFile(m.chat, stream, `${vid.title}.mp4`, `
-        ▢ *Quality*: ${q}
-        ▢ *Size*: Unknown
-      `, m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument });
-      m.react(done);
-    } catch {
-      m.reply(`🚫 Error`);
+     await conn.loadingMsg(m.chat, '📥BAIXANDO', ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ COMPLETO!' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
+	  if(!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /2$/.test(command)), `
+ ≡  *FG YTDL 2*
+  
+*📌${mssg.title}* : ${title}
+*🎞️${mssg.quality}* : ${quality}
+*⚖️${mssg.size}* : ${size}
+`.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
+		m.react(done) 
+		
+		 } catch (error) {
+        m.reply(`🚫 ${mssg.error}`)
     }
-  }
-};
+}
+
+}
+handler.help = ['play']
+handler.tags = ['dl']
+handler.command = ['play', 'musica', 'song']
+
+export default handler
