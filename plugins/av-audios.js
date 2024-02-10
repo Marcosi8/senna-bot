548e5d208325a039c16ba3592484a5ad495c86fc
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-10 17:39:27 -0300

    Update av-audios.js

--- plugins/av-audios.js
+++ plugins/av-audios.js
@@ -16,7 +16,7 @@ export default handler
 let audioMsg = {
   'boa noite|noite|sono|cansado|dormir|trabalhar|trabalho': 'https://g.top4top.io/m_2961taxle0.mp3',
   'bom dia|bom dia bot|bom dia pessoal|bomdia|bom dia gente|bom diaa|bundia|bom dia a todos': 'https://f.top4top.io/m_2961uuv7l0.mp3',
-  'robo|o bot|eae': 'o bot sou eu',
+  'robo|o bot|eae': 'https://c.top4top.io/m_29613e80q1.mp3|https://d.top4top.io/m_2961bgor02.mp3',
   'verdade|mentira|tenho namorada|é mesmo|mentiroso|eu menti|sim': 'https://f.top4top.io/m_2961e8ou64.mp3',
   'paulo|nidney|samuel|jao|alif': 'https://c.top4top.io/m_29613e80q1.mp3',
   '@558881647724': 'https://d.top4top.io/m_2961bgor02.mp3'
