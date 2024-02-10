083b34e38c8a09e1822843619e2d6fd547ae685d
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-10 16:24:48 -0300

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,23 +1,18 @@
-
 import yts from 'yt-search'
-import fg from 'api-dylux'
+import axios from 'axios' // Importando o Axios para fazer requisições HTTP
 
 let limit = 320
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
   
     if (!text) throw `🤔 Você quer que música? ${mssg.example} *${usedPrefix + command}* queen, don't stop me now`
-    
     let chat = global.db.data.chats[m.chat]
     let res = await yts(text)
     let vid = res.videos[0]
-    
     if (!vid) throw `❗️ Vídeo/Audio não encontrado`
-    
-    let isVideo = /vid$/.test(command)
     m.react('🎧') 
   
     let play = `
-	≡ *YT MUSIC*
+    ≡ *YT MUSIC*
 ┌──────────────
 ▢ 📥 *${mssg.title}:* ${vid.title}
 ▢ 📆 *${mssg.aploud}:* ${vid.ago}
@@ -26,28 +21,17 @@ let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, comman
 └──────────────
 
 _Enviando..._` 
-    
     conn.sendFile(m.chat, vid.thumbnail, 'play', play, m, null, rcanal)
   
-    let q = isVideo ? '360p' : '128kbps' 
     try {
-        let yt = await (isVideo ? fg.ytv : fg.yta)(vid.url, q)
-        let { title, dl_url, quality, size, sizeB } = yt
-        let isLimit = limit * 1024 < sizeB 
-
-        await conn.loadingMsg(m.chat, '📥 BAIXANDO', ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ COMPLETO!' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
-     
-        if (!isLimit) {
-            conn.sendFile(m.chat, dl_url, title + '.' + (isVideo ? 'mp4' : 'm4a'), `
-▢ *🎞️Qualidade* : ${quality}
-▢ *⚖️Peso* : ${size}
-`.trim(), m, false, { mimetype: isVideo ? 'video/mp4' : 'audio/m4a', asDocument: chat.useDocument })
-            m.react(done) 
-        } else {
-            throw `⚠️ Limite de tamanho excedido!`
-        }
+        let apiResponse = await axios.get(`https://www.yt-download.org/api/button/mp3/${vid.videoId}`)
+        if (!apiResponse.data.link) throw 'Link de download não encontrado'
+        
+        let audioUrl = apiResponse.data.link
+        await conn.sendFile(m.chat, audioUrl, 'audio.mp3', `🎵 ${vid.title}`, m)
+        m.react('✅') // Reage com um emoji indicando que o processo foi concluído com sucesso
     } catch (error) {
-        m.reply(`🚫 ${mssg.error}: ${error}`)
+        m.reply(`❌ Ocorreu um erro ao processar a música: ${error}`)
     }
 }
 
