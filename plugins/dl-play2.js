7b0e6c52e20edd944f65cace068300e593ee911b
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-10 12:27:35 -0300

    Update dl-play2.js

--- plugins/dl-play2.js
+++ plugins/dl-play2.js
@@ -1,69 +1,56 @@
+import yts from 'yt-search';
+import fg from 'api-dylux';
+import { youtubedl, youtubedlv2 } from '@bochilteam/scraper';
 
-import yts from 'yt-search'
-import fg from 'api-dylux'
-import { youtubedl, youtubedlv2 } from '@bochilteam/scraper'
-let limit = 320
+let limit = 320;
+
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
-  
-    if (!text) throw `🤔 Você quer que música? ${mssg.example} *${usedPrefix + command}* queen, don't stop me now`
-  let chat = global.db.data.chats[m.chat]
-  let res = await yts(text)
-  //let vid = res.all.find(video => video.seconds < 3600)
-  let vid = res.videos[0]
-  if (!vid) throw `❗️ Vídeo/Audio não encontrado`
-  let isVideo = /vid$/.test(command)
-  m.react('🎧') 
-  
-  let play = `
-	≡ *YT MUSIC*
-┌──────────────
-▢ 📥 *${mssg.title}:* ${vid.title}
-▢ 📆 *${mssg.aploud}:* ${vid.ago}
-▢ ⏱️ *${mssg.duration}:* ${vid.timestamp}
-▢ 👀 *${mssg.views}:* ${vid.views.toLocaleString()}
-└──────────────
+    try {
+        if (!text) throw `🤔 Você quer uma música? ${mssg.example} *${usedPrefix + command}* Queen, Don't Stop Me Now`;
 
-_Enviando..._` 
-conn.sendFile(m.chat, vid.thumbnail, 'play', play, m, null, rcanal)
-  
-  let q = isVideo ? '360p' : '128kbps' 
-try {
-  let yt = await (isVideo ? fg.ytv : fg.yta)(vid.url, q)
-  let { title, dl_url, quality, size, sizeB } = yt
-  let isLimit = limit * 1024 < sizeB 
+        let res = await yts(text);
+        let vid = res.videos[0];
 
-     await conn.loadingMsg(m.chat, '📥 BAIXANDO', ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ COMPLETO!' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
-     
-	  if(!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /vid$/.test(command)), `
-▢ *🎞️Qualidade* : ${quality}
-▢ *⚖️Peso* : ${size}
-`.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
-		m.react(done) 
-  } catch {
-  try {
-//  let q = isVideo ? '360p' : '128kbps' 
-  let yt = await (isVideo ? fg.ytmp4 : fg.ytmp3)(vid.url, q)
-  let { title, dl_url, quality, size, sizeB } = yt
-  let isLimit = limit * 1024 < sizeB 
+        if (!vid) throw `❗️ Vídeo/Audio não encontrado`;
 
-     await conn.loadingMsg(m.chat, '📥BAIXANDO', ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ COMPLETO!' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m)
-	  if(!isLimit) conn.sendFile(m.chat, dl_url, title + '.mp' + (3 + /2$/.test(command)), `
- ≡  *FG YTDL 2*
-  
-*📌${mssg.title}* : ${title}
-*🎞️${mssg.quality}* : ${quality}
-*⚖️${mssg.size}* : ${size}
-`.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg', asDocument: chat.useDocument })
-		m.react(done) 
-		
-		 } catch (error) {
-        m.reply(`🚫 ${mssg.error}`)
+        m.react('🎧');
+
+        let play = `
+            ≡ *YT MUSIC*
+            ┌──────────────
+            ▢ 📥 *${mssg.title}:* ${vid.title}
+            ▢ 📆 *${mssg.aploud}:* ${vid.ago}
+            ▢ ⏱️ *${mssg.duration}:* ${vid.timestamp}
+            ▢ 👀 *${mssg.views}:* ${vid.views.toLocaleString()}
+            └──────────────
+
+            _Enviando..._
+        `;
+        
+        conn.sendMessage(m.chat, vid.thumbnail, 'play', play);
+
+        let q = /vid$/.test(command) ? '360p' : '128kbps';
+        let yt = await (isVideo ? fg.ytv : fg.yta)(vid.url, q);
+        let { title, dl_url, quality, size, sizeB } = yt;
+        let isLimit = limit * 1024 < sizeB;
+
+        await conn.loadingMsg(m.chat, '📥 BAIXANDO', ` ${isLimit ? `≡  *FG YTDL*\n\n▢ *⚖️${mssg.size}*: ${size}\n▢ *🎞️${mssg.quality}*: ${quality}\n\n▢ _${mssg.limitdl}_ *+${limit} MB*` : '✅ COMPLETO!' }`, ["▬▭▭▭▭▭", "▬▬▭▭▭▭", "▬▬▬▭▭▭", "▬▬▬▬▭▭", "▬▬▬▬▬▭", "▬▬▬▬▬▬"], m);
+
+        if (!isLimit) {
+            conn.sendMessage(m.chat, dl_url, `
+                ▢ *🎞️Qualidade* : ${quality}
+                ▢ *⚖️Peso* : ${size}
+            `.trim(), m, false, { mimetype: isVideo ? '' : 'audio/mpeg' });
+            
+            m.react('done');
+        }
+    } catch (error) {
+        m.reply(`🚫 ${mssg.error}`);
     }
-}
+};
 
-}
-handler.help = ['play']
-handler.tags = ['dl']
-handler.command = ['play', 'musica', 'song']
+handler.help = ['play'];
+handler.tags = ['dl'];
+handler.command = ['play', 'musica', 'song'];
 
-export default handler
+export default handler;
