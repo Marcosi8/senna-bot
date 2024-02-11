f53438ff7d8126207d4c7139e8a186d42e59c1a6
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-11 18:57:54 -0300

    Update dl-yts.js

--- plugins/dl-yts.js
+++ plugins/dl-yts.js
@@ -1,7 +1,7 @@
 
 import yts from 'yt-search'
 let handler = async (m, {conn, text }) => {
-  if (!text) throw `✳️ ${mssg.search('YouTube')}`
+  if (!text) throw `🤔 ${mssg.search('YouTube')}`
   let results = await yts(text)
 	let tes = results.videos
 let teks = tes.map(v => `
@@ -13,8 +13,8 @@ let teks = tes.map(v => `
 `.trim()).join('\n________________________\n\n')
 	conn.sendFile(m.chat, tes[0].image, 'yts.jpeg', teks, m)
 }
-handler.help = ['ytsearch'] 
-handler.tags = ['dl']
-handler.command = ['ytsearch', 'yts'] 
+handler.help = ['yts] 
+handler.tags = ['dl', 'prime']
+handler.command = ['ytsearch', 'yts', 'pesquisayt'] 
 
 export default handler
