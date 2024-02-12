4319d7f5e9953f272db7144beb375b7ae5ed68bb
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-12 19:43:08 -0300

    Update tools-lyrics.js

--- plugins/tools-lyrics.js
+++ plugins/tools-lyrics.js
@@ -2,21 +2,22 @@
 import fg from 'api-dylux'
 let handler = async (m, {conn, text }) => {
   let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : ''
-   if (!teks) throw `✳️ ${mssg.notext}`
+  m.react('❤️‍🔥');
+if (!teks) throw `🫠 _De que música você quer a letra?_`
    try {
  let res = await fg.lyrics(text);
- let mes = `▢ *${res.title}*
-▢ *${res.artist}*
+ let mes = `▢ 🎷 *${res.title}*
+▢ 👨‍🎤 *${res.artist}*
 
-▢ ${res.lyrics}`;
+▢ 🖋 ${res.lyrics}`;
     conn.sendFile(m.chat, res.thumb, 'img.png', mes, m);
 } catch (e) {
 	m.react(error)
 } 
 
 }
-handler.help = ['lyrics']
-handler.tags = ['tools']
+handler.help = ['letra']
+handler.tags = ['tools', 'prime']
 handler.command = ['letra', 'lyrics', 'letras'] 
 
 export default handler
