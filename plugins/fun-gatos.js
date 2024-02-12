9a536447c4dfe34ffc3cd2e73005e69d5cd557cf
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-12 16:35:02 -0300

    Update fun-gatos.js

--- plugins/fun-gatos.js
+++ plugins/fun-gatos.js
@@ -1,26 +1,28 @@
 function handler(m, { conn, groupMetadata }) {
-    let ps = groupMetadata.participants.map(v => v.jid);
-    let lindos = [];
-    while (lindos.length < 3) {
-        let p = ps[Math.floor(Math.random() * ps.length)];
-        if (!lindos.includes(p)) {
-            lindos.push(p);
-        }
+  let ps = groupMetadata.participants.map(v => v.jid);
+  let lindos = [];
+  while (lindos.length < 3) {
+    let p = ps[Math.floor(Math.random() * ps.length)];
+    if (!lindos.includes(p)) {
+      lindos.push(p);
     }
-    let mentions = lindos.map(p => ({ "tag": `@${p.split("@")[0]}`, "id": p }));
-    let message = `
-        Marque os três mais lindos do grupo! ❤️
-    `;
-    mentions.forEach((mention, index) => {
-        let beleza = (Math.random() * 100).toFixed(2);
-        message += `\n${mention.tag} - ${beleza}%`;
-    });
-    conn.sendMessage(m.chat, message, null, { mentions });
+  }
+  let mentions = lindos.map(p => ({ "tag": `@${p.split("@")[0]}`, "id": p }));
+  let message = `
+    Marque os três mais lindos do grupo! ❤️
+  `;
+  mentions.forEach((mention, index) => {
+    let beleza = (Math.random() * 100).toFixed(2);
+    message += `\n${mention.tag} - ${beleza}%`;
+  });
+  
+  // Corrige o uso de `conn.reply` para resposta à mensagem recebida.
+  conn.reply(m, message, null, { mentions });
 }
 
-handler.help = ['marcarlindos'];
+handler.help = ['toplindo'];
 handler.tags = ['fun'];
-handler.command = ['marcarlindos'];
+handler.command = ['toplindo'];
 handler.group = true;
 
 export default handler;
