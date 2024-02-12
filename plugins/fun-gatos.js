2ed7155d3d6f517f3ca7bcf2f454f76c4d0ff7e1
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-12 15:41:46 -0300

    Update fun-gatos.js

--- plugins/fun-gatos.js
+++ plugins/fun-gatos.js
@@ -1,26 +1,21 @@
 function handler(m, { conn, groupMetadata }) {
-    let ps = groupMetadata.participants.map(v => v.jid);
+    let participants = groupMetadata.participants;
     let lindos = [];
     while (lindos.length < 3) {
-        let p = ps[Math.floor(Math.random() * ps.length)];
-        if (!lindos.includes(p)) {
-            lindos.push(p);
+        let randomParticipant = participants[Math.floor(Math.random() * participants.length)];
+        if (!lindos.includes(randomParticipant)) {
+            lindos.push(randomParticipant);
         }
     }
-    let mentions = lindos.map(p => ({ "tag": toM(p), "id": p }));
-    m.reply(`
-        Marque os três mais lindos do grupo! ❤️
-        ${mentions.map(m => m.tag).join('\n')}
+    let mentions = lindos.map(p => ({ "tag": `@${p.split("@")[0]}`, "id": p }));
+    conn.sendMessage(m.chat, `
+        Marque os três mais lindos do grupo! ❤️\n${mentions.map(m => m.tag).join('\n')}
     `, null, { mentions });
 }
 
 handler.help = ['toplindos'];
-handler.tags = ['fun', 'prime'];
-handler.command = ['toplindos', 'maislindos'];
+handler.tags = ['fun'];
+handler.command = ['toplindos'];
 handler.group = true;
 
 export default handler;
-
-function toM(f) {
-    return '@' + f.split('@')[0];
-}
