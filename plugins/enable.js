ed187fd083269b4bbf81d82cd5b5e9f65ff59f19
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-11 21:17:11 -0300

    Update enable.js

--- plugins/enable.js
+++ plugins/enable.js
@@ -36,19 +36,9 @@ let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isR
        }
        chat.detect = isEnable
      break
-}
+     }
        chat.detect = isEnable
      break
- 
-      case 'autosticker':
-      if (m.isGroup) {
-        if (!(isAdmin || isOwner)) {
-          global.dfail('admin', m, conn)
-          throw false
-        }
-      }
-      chat.autosticker = isEnable
-      break
  
       case 'antispam':
       if (m.isGroup) {
