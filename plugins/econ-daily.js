a391a81cbb7d18b74ffb7f14a7772a26de6b285f
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-13 12:36:12 -0300

    Atualizar o econ-daily.js

--- plugins/econ-daily.js
+++ plugins/econ-daily.js
@@ -11,9 +11,9 @@ let handler = async (m, {conn}) => {
 *${mssg.money}* : +${free.toLocaleString()} 🪙`)
   user.lastclaim = new Date * 1
 }
-handler.help = ['daily']
+handler.help = ['presente']
 handler.tags = ['econ']
-handler.command = ['daily', 'claim'] 
+handler.command = ['daily', 'claim', 'presente'] 
 
 
 export default handler
