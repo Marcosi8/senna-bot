b77c98985745ec26734677ec6da80d98e2633a1d
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-12 22:42:33 -0300

    Update tools-correioanon.js

--- plugins/tools-correioanon.js
+++ plugins/tools-correioanon.js
@@ -1,37 +1,43 @@
 let handler = async (m, { text, conn }) => {
   try {
     // Extrai o número e a mensagem do texto
-    // Usa o caractere '|' como separador
-    let parts = (text ?? '').split('|').map(v => v.trim());
-    let number = parts.shift(); // Remove e retorna o primeiro elemento do array (o número)
-    let message = parts.join('|'); // Junta os elementos restantes para formar a mensagem
+    // Usa a vírgula como separador
+    const parts = text.split(',').map(v => v.trim());
+    const number = parts[0];
+    const message = parts.slice(1).join(',').trim();
     
     // Valida o número
-    if (!number || !/^\+?\d{10,15}$/.test(number)) throw new Error('Número inválido. Certifique-se de usar o formato: _+5588xxxxxxxx_');
+    if (!number || !/^\+?\d{10,15}$/.test(number)) {
+      throw new Error('*Número inválido. Certifique-se de usar o formato:* _+5588xxxxxxxx_');
+    }
     
     // Valida a mensagem
-    if (!message) throw new Error('Mensagem vazia');
+    if (!message) {
+      throw new Error('Mensagem vazia');
+    }
     
     // Limita o tamanho da mensagem
-    if (message.length > 90) throw new Error('Mensagem muito longa');
+    if (message.length > 90) {
+      throw new Error('Mensagem muito longa');
+    }
     
     // Cria a mensagem com aviso de anonimato
-    message = '[AVISO: Esta é uma mensagem de correio anônimo. O remetente não é o número do bot, mas outro usuário que usou o plugin anonimo.]\n\n' + message;
+    const anonymousMessage = '[AVISO: Esta é uma mensagem de correio anônimo. O remetente não é o número do bot, mas outro usuário que usou o plugin anonimo.]\n\n' + message;
     
     // Envia a mensagem
-    await conn.sendMessage(number, message, 'conversation');
+    await conn.sendMessage(number, anonymousMessage, 'conversation');
     
     // Retorna confirmação para o usuário
     m.reply('Mensagem enviada com sucesso');
   } catch (error) {
     // Trata o erro e informa o usuário
-    m.reply(`*Erro ao enviar a mensagem:* ${error.message}`);
+    m.reply(`Erro ao enviar a mensagem: ${error.message}`);
   }
 };
 
 handler.help = ['anonimo'];
 handler.tags = ['fun'];
 handler.command = ['anonimo'];
-handler.group = true;
+handler.group = false;
 
 export default handler;
