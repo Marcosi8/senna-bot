54a787be239ad59e9a4480968ed8d2069cc8d083
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-13 20:29:45 -0300

    Atualizar o plugins

---
+++ plugins/tools-pvanonimo.js
@@ -0,0 +1,36 @@
+async function handler(m, { command, args }) {
+    command = command.toLowerCase()
+    switch (command) {
+        case 'correio': {
+            // Verifica se foram fornecidos argumentos corretos
+            if (!args || args.length < 2) return reply(`Exemplo: ${prefixo + command} +55 00.../Oi amor, sdds`)
+
+            // Extrai o número e a mensagem do argumento
+            let txt1 = args[0].replace(/\D/g,'');
+            let txt2 = args.slice(1).join(' ');
+
+            // Verifica se o número e a mensagem foram fornecidos
+            if (!txt1) return reply('Cadê o número da pessoa?')
+            if (!txt2) return reply('Cadê a mensagem do correio??')
+
+            // Chama a função para enviar a mensagem
+            let [result] = await Ay.onWhatsApp(txt1)
+            if (!result) return reply(`Número inválido`)
+
+            // Monta a mensagem a ser enviada
+            let bla = `┞┧Msg: ${txt2}`
+
+            // Envia a mensagem
+            Ay.sendMessage(result.jid, { text: bla })
+
+            // Retorna a confirmação de envio
+            reply(`Mensagem enviada com sucesso para wa.me/${result.jid.split("@")[0]}`)
+            break;
+        }
+    }
+}
+handler.command = ['pvanonimo']
+handler.tags = ['prime']
+handler.help = ['pvanonimo']
+handler.limit = false
+module.exports = handler
