03ad35fd2ad5290a0ea9e7f529e36c7f783abdfb
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-13 02:19:24 -0300

    Atualizar o tools-correioanon.js

--- plugins/tools-correioanon.js
+++ plugins/tools-correioanon.js
@@ -21,6 +21,12 @@ async function handler(m, { usedPrefix, command }) {
                 room.b = m.sender
                 room.state = 'CHATTING'
                 await this.sendMessage(m.chat, { text: "🎉 *Você foi conectado a um chat anônimo.*"}, { quoted: m })
+
+                // Enviar mensagem para todos os grupos
+                let groups = Object.values(this.chats).filter(chat => chat.jid.endsWith('@g.us'))
+                for (let group of groups) {
+                    await this.sendMessage(group.jid, { text: "🎉 *Alguém se conectou a um chat anônimo!*"})
+                }
             } else {
                 let id = + new Date
                 this.anonymous[id] = {
@@ -35,19 +41,30 @@ async function handler(m, { usedPrefix, command }) {
                         return who === this.a ? this.b : who === this.b ? this.a : ''
                     },
                 }
-                await this.sendMessage(m.chat, { text: "🚦 _*Você está na fila para um chat anônimo, aguarde até alguem se conectar!*_ "}, { quoted: m })
-            }
-            // Envie a mensagem para todos os grupos
-            let groups = Object.values(this.chats).filter(chat => chat.jid.endsWith('@g.us'))
-            for (let group of groups) {
-                await this.sendMessage(group.jid, { text: "🎉 *Alguém entrou em um chat anônimo!*"})
+                await this.sendMessage(m.chat, { text: "🚦 _*Você está na fila para um chat anônimo, aguarde até alguém se conectar!*_ "}, { quoted: m })
             }
             break
         }
+        case 'anonimoinfo': {
+            let anonimoinfoText = `👤 *O chat Anônimo funciona apenas no privado do Bot.*
+👥 Consiste em usar o número do Bot para falar com outras pessoas, ou seja, as duas pessoas estarão escrevendo ao mesmo tempo no chat privado do Bot, dessa forma, nenhuma 🔒 das duas pessoas pode ver o número, foto, usuário, descrição, etc...🔒
+✨ Para usar essa função, siga estas etapas:
+⚡️ Acesse o chat privado do Bot
+⚡️ Digite o seguinte comando: *#start*
+✅ Depois de fazer isso, basta ter paciência até que outra pessoa use o mesmo comando (#start) para serem vinculados pelo número do Bot e começar a interagir.
+✅ Se deixar o #start ativado, terá mais chances de interagir com a outra pessoa de forma anônima.
+🚪 Se desejar sair do chat anônimo, use o seguinte comando: *#leave*
+✅ Dessa forma, você deixará de estar no chat anônimo do Bot.
+❗ Não nos responsabilizamos pelo mau uso dessa função do Bot.`;
+            await this.sendMessage(m.chat, { text: anonimoinfoText }, { quoted: m });
+            break;
+        }
     }
 }
-handler.help = ['start', 'leave', 'next']
-handler.tags = ['chat']
-handler.command = ['start', 'leave', 'next']
-handler.private = false
-export default handler
+
+handler.help = ['start', 'leave', 'next', 'anonimoinfo'];
+handler.tags = ['chat'];
+handler.command = ['start', 'leave', 'next', 'anonimoinfo'];
+handler.private = false;
+
+export default handler;
