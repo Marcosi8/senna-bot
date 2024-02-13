01f3c8cf44b2554c48be5e55722ed84f6a99be42
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-12 22:19:51 -0300

    Update tools-correioanon.js

--- plugins/tools-correioanon.js
+++ plugins/tools-correioanon.js
@@ -1,20 +1,25 @@
 let handler = async (m, { text, conn }) => {
-  // Extrai o número e a mensagem do texto
-  // Usa o caractere | como separador
-  let [number, ...parts] = (text ?? '').split('|').map(v => v.trim());
-  let message = parts.join('|');
-
   try {
+    // Extrai o número e a mensagem do texto
+    // Usa o caractere | como separador
+    let [number, ...parts] = (text ?? '').split('|').map(v => v.trim());
+    let message = parts.join('|');
+    
     // Valida o número
     if (!number || !/^\+?\d{10,15}$/.test(number)) throw new Error('*Número inválido. Certifique-se de usar o formato:* _+5588xxxxxxxx_');
+    
     // Valida a mensagem
     if (!message) throw new Error('Mensagem vazia');
+    
     // Limita o tamanho da mensagem
     if (message.length > 90) throw new Error('Mensagem muito longa');
+    
     // Cria a mensagem com aviso de anonimato
     message = '[AVISO: Esta é uma mensagem de correio anônimo. O remetente não é o número do bot, mas outro usuário que usou o plugin anonimo.]\n\n' + message;
+    
     // Envia a mensagem
     await conn.sendMessage(number, message, 'conversation');
+    
     // Retorna confirmação para o usuário
     m.reply('Mensagem enviada com sucesso');
   } catch (error) {
