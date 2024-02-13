a8f437c8c098802ecc1c22a717c94c852b87bffa
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-13 10:46:55 -0300

    Atualizar o plugins

---
+++ plugins/filmes.js
@@ -0,0 +1,77 @@
+/* Creado/adaptado por Bruno Sobrino (https://github.com/BrunoSobrino) */
+
+import fetch from 'node-fetch';
+import axios from 'axios';
+import {load} from 'cheerio';
+
+const handler = async (m, {text, usedPrefix, command, conn}) => {
+  if (!text) throw `️Insira o nome do filme ou série que deseja pesquisar.\nExemplo: ${usedPrefix + command} O Gato com Botas`;
+
+  let aaaa;
+  let img;
+  
+  try {
+    aaaa = await searchC(text);
+    img = 'https://cinefilosoficial.com/wp-content/uploads/2021/07/cuevana.jpg';
+  } catch {
+    aaaa = await searchP(text);
+    img = 'https://elcomercio.pe/resizer/RJM30xnujgfmaODGytH1rRVOrAA=/400x0/smart/filters:format(jpeg):quality(75)/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/BJ2L67XNRRGHTFPKPDOEQ2AH5Y.jpg';
+  }
+  
+  if (aaaa == '') throw `️Não foi possível encontrar resultados para "${text}".`;
+
+  const res = await aaaa.map((v) => `*🎬 • Título:* ${v.title}\n*🍿 • Link:* ${v.link}`).join`\n\n───────────────\n\n`;
+
+  const helpMessage = `*💫 • Como usar:*\nUse este comando seguido pelo nome do filme ou série que deseja pesquisar.\nExemplo: ${usedPrefix + command} O Gato com Botas`;
+
+  const ads = `*💫 • Mais um anúncio aqui:*\nhttps://block-this.com/block-this-latest.apk\n\n≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣≣\n\n`;
+
+  conn.sendMessage(m.chat, {image: {url: img}, caption: ads + res}, {quoted: m});
+};
+
+handler.tags = ['prime'];
+handler.command = ['filme', 'pelisplus'];
+handler.help = ['cuevana', 'pelisplus <texto>'];
+
+export default handler;
+
+const safeLoad = async (url, options = {}) => {
+  try {
+    const {data: pageData} = await axios.get(url, options);
+    const $ = load(pageData);
+    return $;
+  } catch (err) {
+    if (err.response) {
+      throw new Error(err.response.statusText);
+    }
+    throw err;
+  }
+};
+
+async function searchC(query, numberPage = 1) {
+  const $ = await safeLoad(`https://cuevana3.mu/page/${numberPage}/`, { params: {s: query}});
+  const resultSearch = [];
+  
+  $('.results-post > article').each((_, e) => {
+    const element = $(e);
+    const title = element.find('header > h2').text();
+    const link = element.find('.lnk-blk').attr('href');
+    resultSearch.push({title: title, link: link});
+  });
+  
+  return resultSearch;
+}
+
+async function searchP(query, numberPage = 1) {
+  const $ = await safeLoad(`https://pelisplushd.cx/search/`, { params: {s: query, page: numberPage}});
+  const resultSearch = [];
+  
+  $('a[class^=\'Posters\']').each((_, e) => {
+    const element = $(e);
+    const title = element.find('.listing-content > p').text();
+    const link = element.attr('href');
+    resultSearch.push({title: title, link: link});
+  });
+  
+  return resultSearch;
+}
