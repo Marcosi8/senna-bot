48e6ef3aae47031bb95a06e6fbbb2172e7ee0d4c
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-13 20:44:49 -0300

    Atualizar o tools-pvanonimo.js

--- plugins/tools-pvanonimo.js
+++ plugins/tools-pvanonimo.js
@@ -1,36 +1,20 @@
-async function handler(m, { command, args }) {
-    command = command.toLowerCase()
-    switch (command) {
-        case 'pvanonimo': {
-            // Verifica se foram fornecidos argumentos corretos
-            if (!args || args.length < 2) return reply(`Exemplo: ${prefixo + command} +55 00.../Oi amor, sdds`)
+let handler = async (m) => {
+    let txt = args.join(" ");
+    if (!txt) return reply(`Exemplo: ${prefix + command} +55 00.../Oi amor, sdds`);
+    let txt1 = txt.split("/")[0].replace(/\D/g,'');
+    let txt2 = txt.split("/")[1];
+    if (!txt1) return reply('Cadê o número da pessoa?');
+    if (!txt2) return reply('Cadê a mensagem do correio??');
+    let [result] = await this.onWhatsApp(txt1); // Supondo que this.onWhatsApp seja o método para encontrar informações do número no seu bot.
+    if (!result) return reply(`Número inválido`);
+    let bla = `
+┞┧Msg: ${txt2}`;
+    await this.sendMessage(result.jid, { text: bla }); // Supondo que this.sendMessage seja o método para enviar mensagens no seu bot.
+    reply(`Mensagem enviada com sucesso para wa.me/${result.jid.split("@")[0]}`);
+};
 
-            // Extrai o número e a mensagem do argumento
-            let txt1 = args[0].replace(/\D/g,'');
-            let txt2 = args.slice(1).join(' ');
+handler.help = ['pvcorreio'];
+handler.tags = ['tools', 'prime'];
+handler.command = /^pvcorreio|mail$/i;
 
-            // Verifica se o número e a mensagem foram fornecidos
-            if (!txt1) return reply('Cadê o número da pessoa?')
-            if (!txt2) return reply('Cadê a mensagem do correio??')
-
-            // Chama a função para enviar a mensagem
-            let [result] = await Ay.onWhatsApp(txt1)
-            if (!result) return reply(`Número inválido`)
-
-            // Monta a mensagem a ser enviada
-            let bla = `┞┧Msg: ${txt2}`
-
-            // Envia a mensagem
-            Ay.sendMessage(result.jid, { text: bla })
-
-            // Retorna a confirmação de envio
-            reply(`Mensagem enviada com sucesso para wa.me/${result.jid.split("@")[0]}`)
-            break;
-        }
-    }
-}
-handler.command = ['pvanonimo']
-handler.tags = ['prime']
-handler.help = ['pvanonimo']
-handler.limit = false
-module.exports = handler
+export default handler;
