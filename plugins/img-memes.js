291f7fe8f11cf88e9b9e1baa6734367b91e73705
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-13 11:46:18 -0300

    Atualizar o img-memes.js

--- plugins/img-memes.js
+++ plugins/img-memes.js
@@ -1,14 +1,28 @@
+const axios = require('axios');
 
 let handler = async(m, { conn, usedPrefix, command }) => {
-	
-	let img = await conn.getFile(global.API('fgmods', '/api/img/meme', { }, 'apikey'))
-	let meme = img.data
-	m.react("😂")
-      await conn.sendFile(m.chat, meme, '', '', m) 
-      
-}
-handler.help = ['meme']
-handler.tags = ['img']
-handler.command = ['meme', 'memes'] 
+    const options = {
+        method: 'GET',
+        url: 'https://memes-brasileiros1.p.rapidapi.com/dev/memes',
+        headers: {
+            'X-RapidAPI-Key': '14f5625a72mshd4beb354ebf1af4p1752a4jsn11e344cd9c9e',
+            'X-RapidAPI-Host': 'memes-brasileiros1.p.rapidapi.com'
+        }
+    };
 
-export default handler
+    try {
+        const response = await axios.request(options);
+        const memeUrl = response.data.url;
+        m.react("😂");
+        await conn.sendFile(m.chat, memeUrl, '', '', m);
+    } catch (error) {
+        console.error(error);
+        m.reply('Erro ao obter o meme.');
+    }
+}
+
+handler.help = ['meme'];
+handler.tags = ['img', 'prime'];
+handler.command = ['meme', 'memes'];
+
+export default handler;
