2cb8851dc3149d857b1d62419f10c2422df4f1d1
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-20 18:03:47 -0300

    Update tools-openai.js

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -1,22 +1,79 @@
-
-import cheerio from 'cheerio';
 import fetch from 'node-fetch';
-let handler = async (m, { conn, text }) => {
-	
-if (!text) throw `🤖 ${mssg.notext}`;
-m.react('💬')
 
-	try {
-		let gpt = await fetch(global.API('fgmods', '/api/info/openai2', { text }, 'apikey'));
-        let res = await gpt.json()
-        await m.reply(res.result)
-	} catch {
-		m.reply(`🚫 Error: Tente novamente`);
-	}
+let handler = async (m, { text, conn, usedPrefix, command }) => {
+  if (!text && !(m.quoted && m.quoted.text)) {
+    throw `❇Exemplo: ${usedPrefix + command} 𝘸𝘩𝘢𝘵 𝘪𝘴 𝘪𝘴𝘭𝘢𝘮?`;
+  }
 
-}
-handler.help = ['chatgpt <text>']; 
-handler.tags = ['tools', 'prime'];
-handler.command = ['ia', 'ai', 'chatgpt', 'openai', 'gpt'];
+  if (!text && m.quoted && m.quoted.text) {
+    text = m.quoted.text;
+  }
+
+  let pp = princegpt.getRandom()
+  
+  try {
+    m.react(rwait)
+    const { key } = await conn.sendMessage(m.chat, {
+      image: pp,
+      caption: '𝙃𝙤𝙡𝙙 𝙤𝙣 𝙜𝙚𝙩𝙩𝙞𝙣𝙜 𝙮𝙤𝙪𝙧 𝙖𝙣𝙨𝙬𝙚𝙧...'
+    }, {quoted: m})
+    conn.sendPresenceUpdate('composing', m.chat);
+    const prompt = encodeURIComponent(text);
+
+    const guru1 = `https://api.vihangayt.me/tools/chatgpt2?q=Hi`;
+    
+    try {
+      let response = await fetch(guru1);
+      let data = await response.json();
+      let result = data.result;
+
+      if (!result) {
+        
+        throw new Error('No valid JSON response from the first API');
+      }
+
+      await conn.relayMessage(m.chat, {
+        protocolMessage: {
+          key,
+          type: 14,
+          editedMessage: {
+            imageMessage: { caption: result }
+          }
+        }
+      }, {});
+      m.react(done);
+    } catch (error) {
+      console.error('Error from the first API:', error);
+
+  
+      const model = 'llama';
+      const senderNumber = m.sender.replace(/[^0-9]/g, ''); 
+      const session = `GURU_BOT_${senderNumber}`;
+      const guru2 = `https://api.vihangayt.me/tools/chatgpt3?q=Hi`;
+      
+      let response = await fetch(guru2);
+      let data = await response.json();
+      let result = data.completion;
+
+      await conn.relayMessage(m.chat, {
+        protocolMessage: {
+          key,
+          type: 14,
+          editedMessage: {
+            imageMessage: { caption: result }
+          }
+        }
+      }, {});
+      m.react(done);
+    }
+
+  } catch (error) {
+    console.error('Error:', error);
+    throw `*ERROR*`;
+  }
+};
+handler.help = ['chatgpt']
+handler.tags = ['prime']
+handler.command = ['ai', 'gpt', 'chatgpt'];
 
 export default handler;
