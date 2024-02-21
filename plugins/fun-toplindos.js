c2d9dedf06b7003ac516632e42069518f87c8599
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-21 11:08:24 -0300

    Update fun-toplindos.js

--- plugins/fun-toplindos.js
+++ plugins/fun-toplindos.js
@@ -1,41 +1,41 @@
-/* function handler(m, {conn, groupMetadata }) {
+ //function handler(m, {conn, groupMetadata }) {
 //    let now = new Date();
-    let lastUsed = global.db.data.users[m.sender].shipping || 0;
+   // let lastUsed = global.db.data.users[m.sender].shipping || 0;
  //   let elapsedTime = now - lastUsed;
 
   // if (elapsedTime < 1800000) // 30 minutos em milissegundos
      //   throw `🕴 Você pode usar novamente em *${msToTime(1800000 - elapsedTime)}*`;
 
-    let ps = groupMetadata.participants.map(v => v.id);
-    let f = ps.getRandom();
-    let h = ps.getRandom();
-    let g;
+  //  let ps = groupMetadata.participants.map(v => v.id);
+  //  let f = ps.getRandom();
+  //  let h = ps.getRandom();
+  //  let g;
 
-    do {
-        g = ps.getRandom();
-    } while (h === f || h === g);
+  //  do {
+     //   g = ps.getRandom();
+  //  } while (h === f || h === g);
 
-    m.reply(`
-▢ *❗️🚨 ATENÇÃO 🚨❗️ PARA OS MAIS LINDOS DO GRUPO!!👇*
+   // m.reply(`
+//▢ *❗️🚨 ATENÇÃO 🚨❗️ PARA OS MAIS LINDOS DO GRUPO!!👇*
 
-🥇🏳️‍🌈 ${toM(f)}
+//🥇🏳️‍🌈 ${toM(f)}
 
-🥈🫦 ${toM(h)}
+//🥈🫦 ${toM(h)}
 
-🥉🧌 ${toM(g)}
-`, null, { mentions: [f, g] });
+//🥉🧌 ${toM(g)}
+//`, null, { mentions: [f, g] });
 
-    global.db.data.users[m.sender].shipping = now;
-}
+ //   global.db.data.users[m.sender].shipping = now;
+//}
 
-handler.help = ['toplindo'];
-handler.tags = ['fun', 'prime'];
-handler.command = ['toplindo'];
-handler.group = true;
+//handler.help = ['toplindo'];
+//handler.tags = ['fun', 'prime'];
+//handler.command = ['toplindo'];
+//handler.group = true;
 
-export default handler;
+//export default handler;
 
-let toM = f => '@' + f.split('@')[0];
+//let toM = f => '@' + f.split('@')[0];
 
 // function msToTime(duration) {
  //   var milliseconds = parseInt((duration % 1000) / 100),
