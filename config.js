85cb2cc794e56121ab7df4b96f8a5f0ea22f005e
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-21 10:16:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -39,12 +39,12 @@ global.img8 = 'https://i.ibb.co/PWbLKw8/5c9710e0-de56-474e-80a8-2f89f243eaac.jpg
 
 
 //Chatgpt
-global.gpt1 = fs.readFileSync("./src/GPT/gpt1.jpeg")
-global.gpt2 = fs.readFileSync("./src/GPT/gpt2.jpeg")
-global.gpt3 = fs.readFileSync("./src/GPT/gpt3.jpeg")
-global.gpt4 = fs.readFileSync("./src/GPT/gpt4.jpeg")
-global.gpt5 = fs.readFileSync("./src/GPT/gpt4.jpeg")
-global.gpt6 = fs.readFileSync("./src/GPT/gpt4.jpeg")
+global.gpt1 = fs.readFileSync("./src/GPT/Menu4.png")
+global.gpt2 = fs.readFileSync("./src/GPT/Menu4.png")
+global.gpt3 = fs.readFileSync("./src/GPT/Menu4.png")
+global.gpt4 = fs.readFileSync("./src/GPT/Menu4.png")
+global.gpt5 = fs.readFileSync("./src/GPT/Menu4.png")
+global.gpt6 = fs.readFileSync("./src/GPT/Menu4.png")
 
 global.princeMenu = [img, img2, img6, img7, img8] //img9, img13, img14, img15, img16, img17]
 global.princegpt = [gpt1, gpt2, gpt3, gpt4, gpt5, gpt6]
