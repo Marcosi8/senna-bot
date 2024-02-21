309a4f623b9129b582aff8268bafb1960fbbb9d9
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-21 10:28:48 -0300

    Update tools-openai.js

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -20,7 +20,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
     conn.sendPresenceUpdate('composing', m.chat);
     const prompt = encodeURIComponent(text);
 
-    const guru1 = `https://api.vihangayt.me/tools/chatgpt4?q=Hi`;
+    const guru1 = `https://api.vihangayt.me/tools/bard?q=Hi`;
     
     try {
       let response = await fetch(guru1);
@@ -49,7 +49,7 @@ let handler = async (m, { text, conn, usedPrefix, command }) => {
       const model = 'llama';
       const senderNumber = m.sender.replace(/[^0-9]/g, ''); 
       const session = `GURU_BOT_${senderNumber}`;
-      const guru2 = `https://api.vihangayt.me/tools/chatgpt3?q=Hi`;
+      const guru2 = `https://api.vihangayt.me/tools/bard?q=Hi`;
       
       let response = await fetch(guru2);
       let data = await response.json();
