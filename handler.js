36789c9d614e50ef2ef3473399bb8210e447fc68
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 18:30:29 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -496,6 +496,7 @@ export async function participantsUpdate({ id, participants, action }) {
     let chat = global.db.data.chats[id] || {}
     let text = ''
     switch (action) {
+     case 'add':
      case 'ban':
     if (chat.welcome) {
         let groupMetadata = await this.groupMetadata(id) || (conn.chats[id] || {}).metadata;
