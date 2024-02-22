371ce1b35b7a8dfcbc59903151a6dd71748687c3
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 11:48:27 -0300

    Update tools-pvanonimo.js

--- plugins/tools-pvanonimo.js
+++ plugins/tools-pvanonimo.js
@@ -1,11 +1,15 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+    m.react('📬'); // Adicionando reação
+    
+    m.reply('📤 *Enviando seu correio anônimo...*') // Mensagem de envio imediato
+    
     let time = global.db.data.users[m.sender].lastrob + 300000 // 5 minutos em milissegundos
     if (new Date - global.db.data.users[m.sender].lastrob < 300000) 
-        throw `⏱️ *ESPERE* ${msToTime(time - new Date())}\n*PARA QUE POSSA ENVIAR OUTRA MENSAGEM.*`
+        throw `⏱️ *ESPERE* ${msToTime(time - new Date())}\n*NÃO USE ESTE COMANDO COMO SPAMMER, 1 USO POR VEZ.*`
     
     let [nomor, pesan] = text.split('|')
     if (!nomor || !pesan) 
-        throw `*Por favor, forneça o número e a mensagem que deseja enviar.* \n\n*Formato:* \n*📩 ${usedPrefix + command} numero|mensagem*\n\n*Exemplo:* \n*📩 ${usedPrefix + command} 999999999999|Esta é uma mensagem anônima.*`
+        throw `*Por favor, forneça o número e a mensagem que deseja enviar.* \n\n*Formato:* \n*> ${usedPrefix + command} numero|mensagem*\n\n*Exemplo:* \n*> ${usedPrefix + command} 999999999999|Esta é uma mensagem anônima.*`
     
     await delay(10000)
     let fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net'
@@ -13,10 +17,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     await delay(10000)
     await conn.sendMessage(fixedNumber, {
         text: `📩 𝙑𝙤𝙘ê 𝙧𝙚𝙘𝙚𝙗𝙚𝙪 𝙪𝙢𝙖 𝙢𝙚𝙣𝙨𝙖𝙜𝙚𝙢 𝙖𝙣ô𝙣𝙞𝙢𝙖 𝙙𝙚 𝙤𝙪𝙩𝙧𝙤 𝙪𝙨𝙪á𝙧𝙞𝙤. 𝙀𝙨𝙩𝙖 𝙢𝙚𝙣𝙨𝙖𝙜𝙚𝙢 𝙣ã𝙤 𝙛𝙤𝙞 𝙚𝙣𝙫𝙞𝙖𝙙𝙖 𝙥𝙚𝙡𝙤 𝙗𝙤𝙩.\n\n${pesan.trim()}`,
-        quoted: m
+        quoted: m // Quote com a mensagem original
     })
     
-    m.reply(`✅ *Mensagem enviada com sucesso para* ${nomor}!`)
+    m.reply(`📥 *Mensagem enviada com sucesso para* ${nomor}!`) // Confirmação de envio
     
     global.db.data.users[m.sender].lastrob = new Date * 1
 }
@@ -24,6 +28,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 handler.help = ['correio']
 handler.tags = ['prime', 'chat']
 handler.command = ['pvanonimo', 'correio', 'correioanonimo'] 
+
 export default handler 
 
 const delay = time => new Promise(res => setTimeout(res, time))
@@ -37,4 +42,4 @@ function msToTime(duration) {
     minutes = (minutes < 10) ? "0" + minutes : minutes
     seconds = (seconds < 10) ? "0" + seconds : seconds
     return hours + " Hora(s) " + minutes + " Minuto(s)"
-}
+                          }
