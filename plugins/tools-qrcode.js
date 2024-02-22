7a9e551906933956782c2e9bf70c54be8a6e0575
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 09:31:30 -0300

    Update tools-qrcode.js

--- plugins/tools-qrcode.js
+++ plugins/tools-qrcode.js
@@ -1,6 +1,6 @@
 import { toDataURL } from 'qrcode'
 let handler = async (m, { text, conn }) => {
-if (!text) throw `*ESCREVA O TEXTO PARA CRIAR UM QRCODE*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝘾𝙊𝙉𝙑𝙀𝙍𝙏 𝙄𝙉𝙏𝙊 𝙌𝙍 𝘾𝙊𝘿𝙀`
+if (!text) throw `❗️ *ESCREVA O TEXTO PARA CRIAR UM QRCODE*\n\n𝙒𝙍𝙄𝙏𝙀 𝘼 𝙏𝙀𝙓𝙏 𝙏𝙊 𝘾𝙊𝙉𝙑𝙀𝙍𝙏 𝙄𝙉𝙏𝙊 𝙌𝙍 𝘾𝙊𝘿𝙀`
 conn.sendFile(m.chat, await toDataURL(text.slice(0, 2048), { scale: 8 }), 'qrcode.png', m)
 }
 handler.help = ['qrcode'].map(v => 'qr' + v + ' <text>')
