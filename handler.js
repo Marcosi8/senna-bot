7137ded00beba949eb5273cc21424a35a509f85c
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 18:23:23 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -509,15 +509,14 @@ export async function participantsUpdate({ id, participants, action }) {
             } finally {
                 const botTt2 = groupMetadata.participants.find(u => this.decodeJid(u.id) == this.user.jid) || {};
                 const isBotAdminNn = botTt2?.admin === "admin" || false;
-                const messageContent = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || `Bem-vindo, @${user.split('@')[0]}!`).replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || ('😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 😻') :
-                (chat.sBye || this.bye || conn.bye || `Adeus, @${user.split('@')[0]}!`).replace('@subject', await this.getName(id))));
+                const messageContent = (action === 'add' ? (chat.sWelcome || this.welcome || conn.welcome || `Bem-vindo, @${user.split('@')[0]}!`).replace('@subject', await this.getName(id)).replace('@desc', groupMetadata.desc?.toString() || '😻 𝗦𝘂𝗽𝗲𝗿 𝗚𝗮𝘁𝗮𝗕𝗼𝘁-𝗠𝗗 😻') :
+                (chat.sBye || this.bye || conn.bye || `Adeus, @${user.split('@')[0]}!`)).replace('@subject', await this.getName(id));
                 
                 await this.sendFile(id, pp, 'prefil.jpg', messageContent, null, false, { mentions: [user] });
-            
-                }
             }
         }
-        break
+    }
+    break
       
         case 'promote':
             text = (chat.sPromote || this.spromote || conn.spromote || '@user agora é um administrador')
