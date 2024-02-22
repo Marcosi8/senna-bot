2d416f547ea9e2ae9b894e2c6a44bfbd815a0fa1
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 15:01:27 -0300

    Update tools-walink.js

--- plugins/tools-walink.js
+++ plugins/tools-walink.js
@@ -1,23 +1,38 @@
-let handler = async (m, { conn, usedPrefix, text, command }) => {
+async function handler(m, { conn, usedPrefix, text, command }) {
+  console.log('1');
   let waLin = '';
+  console.log('2');
   if (text) {
+    console.log('3');
     waLin = text.replace(/[^0-9]/g, '');
-  } else if (m.quoted) {
+    console.log('4');
+  } else if (m.quoted && m.quoted.sender) {
+    console.log('5');
     waLin = m.quoted.sender.replace(/[^0-9]/g, '');
+    console.log('6');
   } else if (m.mentionedJid && m.mentionedJid[0]) {
+    console.log('7');
     waLin = m.mentionedJid[0].replace(/[^0-9]/g, '');
+    console.log('8');
   } else {
+    console.log('9');
     throw `❗️ *Digite um número, marque um usuário ou mencione um usuário*`;
+    console.log('10');
   }
   const waLink = `https://wa.me/${waLin}`;
-  const message = `*WhatsApp Link:* ${waLink}\n*Número de telefone:* ${waLin}`; // Concatenando o link e o número de telefone 
+  console.log('11');
+  const message = `*WhatsApp Link:* ${waLink}\n*Número de telefone:* ${waLin}`; // Concatenando o link e o número de telefone
+  console.log('12');
+
   conn.sendMessage(m.chat, { text: message, quoted: m, contextInfo: { mentionedJid: [m.sender] } });
-  
+  console.log('13');
+
   m.react('🗃');
+  console.log('14');
 }
 
-handler.help = ['wa'];
-handler.tags = ['tools', 'prime'];
-handler.command = ['wa'];
+const help = ['wa'];
+const tags = ['tools', 'prime'];
+const command = ['wa'];
 
-export default handler;
+export { handler, help, tags, command };
