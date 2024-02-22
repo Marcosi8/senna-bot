710786915eedc6149c876becdcffbf024e12eb74
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 12:27:17 -0300

    Update tools-pvanonimo.js

--- plugins/tools-pvanonimo.js
+++ plugins/tools-pvanonimo.js
@@ -1,41 +1,37 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-    // Verifica se o comando foi usado corretamente
+    // Criado por: https://github.com/Marcosi8
     if (!text || !text.includes('|')) {
-        throw `*Por favor, use o comando corretamente.* \n\n*Formato:* \n*📩 ${usedPrefix + command} numero|mensagem*\n\n*Exemplo:* \n*📩 ${usedPrefix + command} 999999999999|Esta é uma mensagem anônima.*`
+        throw `*Por favor, use o comando corretamente.* \n\n*Formato:* \n*> ${usedPrefix + command} numero|mensagem*\n\n*Exemplo:* \n*> ${usedPrefix + command} 999999999999|Esta é uma mensagem anônima.*`
     }
 
     m.react('📬'); // Adicionando reação
     
-    let time = global.db.data.users[m.sender].lastrob + 300000; // 5 minutos em milissegundos
-    if (new Date - global.db.data.users[m.sender].lastrob < 300000) {
+    let time = global.db.data.users[m.sender].lastrob + 60000; // 1 minutos em milissegundos, ajuste como quiser.
+    if (new Date - global.db.data.users[m.sender].lastrob < 60000) {
         throw `⏱️ *ESPERE* ${msToTime(time - new Date())}\n*NÃO USE ESTE COMANDO COMO SPAMMER, 1 USO POR VEZ.*`;
     }
     
     let [nomor, pesan] = text.split('|');
     
-    await delay(10000); // Aguarda 10 segundos
+    m.reply('📤 Enviando seu correio anônimo...'); // Mensagem de aviso
     
     let fixedNumber = nomor.replace(/[-+<>@]/g, '').replace(/ +/g, '').replace(/^[0]/g, '62') + '@s.whatsapp.net';
     
-    await delay(10000); // Aguarda 10 segundos
-    
     await conn.sendMessage(fixedNumber, {
-        text: `📮 𝗖𝗢𝗥𝗥𝗘𝗜𝗢 𝗔𝗡Ô𝗡𝗜𝗠𝗢 📮\n\n> ${pesan.trim()}\n\n📩 𝙑𝙤𝙘ê 𝙧𝙚𝙘𝙚𝙗𝙚𝙪 𝙪𝙢𝙖 𝙢𝙚𝙣𝙨𝙖𝙜𝙚𝙢 𝙖𝙣ô𝙣𝙞𝙢𝙖 𝙙𝙚 𝙤𝙪𝙩𝙧𝙤 𝙪𝙨𝙪á𝙧𝙞𝙤. 𝙀𝙨𝙩𝙖 𝙢𝙚𝙣𝙨𝙖𝙜𝙚𝙢 𝙣ã𝙤 𝙛𝙤𝙞 𝙚𝙣𝙫𝙞𝙖𝙙𝙖 𝙥𝙚𝙡𝙤 𝙗𝙤𝙩.`,
+        text: `📮 𝗖𝗢𝗥𝗥𝗘𝗜𝗢 𝗔𝗡Ô𝗡𝗜𝗠𝗢 📮\n\n> ${pesan.trim()}\n\n        📩 𝙑𝙤𝙘ê 𝙧𝙚𝙘𝙚𝙗𝙚𝙪 𝙪𝙢𝙖 𝙢𝙚𝙣𝙨𝙖𝙜𝙚𝙢 𝙖𝙣ô𝙣𝙞𝙢𝙖 𝙙𝙚 𝙤𝙪𝙩𝙧𝙤 𝙪𝙨𝙪á𝙧𝙞𝙤. 𝙀𝙨𝙩𝙖 𝙢𝙚𝙣𝙨𝙖𝙜𝙚𝙢 𝙣ã𝙤 𝙛𝙤𝙞 𝙚𝙣𝙫𝙞𝙖𝙙𝙖 𝙥𝙚𝙡𝙤 𝙗𝙤𝙩.`,
         quoted: m // Quote com a mensagem original
     });
     
-    m.reply(`✅ *Mensagem enviada com sucesso para* ${nomor}!`); // Confirmação de envio
+    m.reply(`📥 *Correio enviada com sucesso para* ${nomor}!`); // Confirmação de envio
     
     global.db.data.users[m.sender].lastrob = new Date * 1;
 }
 
-handler.help = ['spam'];
-handler.tags = ['prime'];
-handler.command = ['spamar', 'spam'];
+handler.help = ['correio']
+handler.tags = ['prime', 'chat']
+handler.command = ['pvanonimo', 'correio', 'correioanonimo'] 
 
 export default handler;
-
-const delay = time => new Promise(res => setTimeout(res, time));
 
 function msToTime(duration) {
     var milliseconds = parseInt((duration % 1000) / 100),
@@ -46,4 +42,4 @@ function msToTime(duration) {
     minutes = (minutes < 10) ? "0" + minutes : minutes
     seconds = (seconds < 10) ? "0" + seconds : seconds
     return hours + " Hora(s) " + minutes + " Minuto(s)"
-}
+             }
