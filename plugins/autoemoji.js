913498053ff17242107a2b373bc8669f2ca93275
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 20:05:52 -0300

    Create autoemoji.js

---
+++ plugins/autoemoji.js
@@ -0,0 +1,23 @@
+function handler(m, { conn, args }) {
+  if (args[0] === 'on') {
+    conn.autoreactEnabled = true;
+    conn.reply(m.chat, 'Auto-reaction ativado!', m);
+  } else if (args[0] === 'off') {
+    conn.autoreactEnabled = false;
+    conn.reply(m.chat, 'Auto-reaction desativado!', m);
+  } else {
+    conn.reply(m.chat, 'Use *autoreact on* ou *autoreact off*.', m);
+  }
+}
+
+handler.help = ['autoreact on/off'];
+handler.tags = ['prime'];
+handler.command = /^(autoreact)$/i;
+handler.group = true;
+
+function pickRandomEmoticon() { 
+  const emoticons = ["😺", "🪨", "🚀", "🏂", "😼", "🐀", "🙀", "🚦", "🥳", "🇧🇷", "😏", "😳", "🥵", "🤯", "😱", "😨", "🤫", "🥴", "🇧🇷", "🤑", "🤠", "🤖", "🤝", "💪", "👑", "😚", "👨‍💻", "🐈", "🐆", "🐅", "⚡️", "🌈", "☃️", "🍦", "🌝", "🌛", "🌜", "🍓", "🍎", "🎈", "🪄", "❤️", "👨‍💻", "🚪", "💉", "🥸", "🔌", "💡", "🕴", "🥇", "📂", "🍕", "👰", "😎", "🔥", "🖕", "🫂"];
+  return emoticons[Math.floor(Math.random() * emoticons.length)];
+}
+
+module.exports = handler;
