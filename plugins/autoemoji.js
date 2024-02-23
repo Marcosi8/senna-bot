4b82f151aaf481c2511bef26ec0bb2e6406e9904
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 21:42:37 -0300

    Update autoemoji.js

--- plugins/autoemoji.js
+++ plugins/autoemoji.js
@@ -2,7 +2,6 @@ export function handler(m, { conn, args }) {
   if (args[0] === 'on') {
     conn.autoreactEnabled = true;
     conn.reply(m.chat, 'Auto-reaction ativado!', m);
-    autoReact(m, conn); // Chama a função para reação automática
   } else if (args[0] === 'off') {
     conn.autoreactEnabled = false;
     conn.reply(m.chat, 'Auto-reaction desativado!', m);
@@ -22,7 +21,7 @@ export function pickRandomEmoticon() {
   return emoticons[Math.floor(Math.random() * emoticons.length)];
 }
 
-function autoReact(m, conn) {
+export function autoReact(m, conn) {
   if (!conn.autoreactEnabled) return; // Verifica se a reação automática está ativada
   const emoticon = pickRandomEmoticon(); // Seleciona um emoji aleatório
   conn.sendMessage(m.chat, { text: emoticon, contextInfo: { mentionedJid: [m.sender] } }); // Envia a reação
