058d204f6e701586fe17a338dce9f674a7133403
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 23:11:52 -0300

    Update gp‐chamaram.js

--- plugins/gp‐chamaram.js
+++ plugins/gp‐chamaram.js
@@ -1,23 +1,23 @@
 let handler = async (message, { conn, participants, groupMetadata, args, usedPrefix, text, command }) => {
   if (!text) return message.reply(`Por favor, inclua uma mensagem para enviar aos administradores do grupo.`)
-  const pp = groupMetadata?.profilePicThumbObj?.eurl || './src/avatar_contact.png'
+  const pp = groupMetadata?.profilePicThumbObj?.eurl || './src/admins.jpg'
   const groupAdmins = participants.filter(p => p.isAdmin)
   const listAdmins = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.jid.split('@')[0]}*`).join('\n')
   const owner = groupMetadata.owner || groupAdmins.find(p => p.isAdmin === 'superadmin') || message.chat.split`-`[0] + '@s.whatsapp.net'
   let pesan = args.join` `
-  let formattedMessage = `*Denúncia ou Aviso aos Administradores do Grupo* 🚨\n\n${pesan}`
-  let adminListMessage = `*Lista de Administradores:*\n${listAdmins}\n\n_Envie sua mensagem com confiança, os administradores estão aqui para ajudar!_ 🤝`
+  let formattedMessage = `*Denúncia ou Aviso aos Administradores do Grupo* 🚨\n\n${pesan}\n\n*Lista de Administradores:*\n${listAdmins}\n\n_Envie sua mensagem com confiança, os administradores estão aqui para ajudar!_ 🤝`
   
   try {
-    await conn.sendFile(message.chat, pp, 'admins_message.jpg', Buffer.from(formattedMessage + '\n\n' + adminListMessage), message, false, { mentions: groupAdmins.map(v => v.jid) })
+    await conn.sendFile(message.chat, pp, 'admins_message.jpg', Buffer.from(formattedMessage), message, false, { mentions: groupAdmins.map(v => v.jid) })
   } catch (error) {
     console.error(error)
     message.reply('Ops! Algo deu errado ao enviar a mensagem para os administradores do grupo.')
   }
 }
 
-handler.tags = ['prime'] // Tags para identificar o comando
+handler.tags = ['admin'] // Tags para identificar o comando
 handler.help = ['admins', '@admins', 'dmins'] // Comandos de ajuda
+
 handler.command = /^(admins|@admins|dmins)$/i
 handler.group = true
 export default handler
