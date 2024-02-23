bb7be0940d54be9f188479d7bb46335b5c89c224
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-23 19:27:51 -0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -2,15 +2,29 @@
 let handler = async function (m, { conn, text, usedPrefix }) {
   
 let m2 = `
-≡ Use estes comandos sem prefixo: *${usedPrefix}*
-┌─⊷ *AUDIOS* 
-▢ bom dia 
-▢ fino senhores
-▢ fake
-▢ paulo
-▢ adm
-▢ bot
-└──────────────
+> Lista de comandos
+
+┌─⊷ *ADMIN*
+▢ captcha
+▢ welcome
+▢ antilink
+▢ detect 
+▢ document
+▢ nsfw
+▢ autosticker
+└───────────── 
+┌─⊷ *USERS*
+▢ autolevelup
+▢ chatbot 
+└─────────────
+┌─⊷ *OWNER*
+▢ public
+▢ sopv
+▢ sogp
+└─────────────
+*📌 Exemplo :*
+*${usedPrefix}on* welcome
+*${usedPrefix}off* welcome
 `
     let pp = './src/WhatsApp Video 2024-02-08 at 16.59.44.mp4' 
     /*conn.sendButton(m.chat, m2, mssg.ig, pp, [
@@ -23,6 +37,6 @@ let m2 = `
 
 handler.help = ['menu2']
 handler.tags = ['main']
-handler.command = ['menu2', 'audios'] 
+handler.command = ['menu2', 'setmenu'] 
 
 export default handler
