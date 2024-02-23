924f5fbcb979a93eba9cf5251ba0a0088d698e9c
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 23:54:59 -0300

    Update gp‐chamaram.js

--- plugins/gp‐chamaram.js
+++ plugins/gp‐chamaram.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, participants, groupMetadata, args }) => {
 
     const pp = await conn.profilePictureUrl(m.chat, 'image').catch(_ => null) || './src/avatar_contact.png'
     const groupAdmins = participants.filter(p => p.admin)
-    const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n❗️ ')
+    const listAdmin = groupAdmins.map((v, i) => `${i + 1}. @${v.id.split('@')[0]}`).join('\n⚠️ ')
     const owner = groupMetadata.owner || groupAdmins.find(p => p.admin === 'superadmin')?.id || m.chat.split`-`[0] + '@s.whatsapp.net'
 
     let text = `
@@ -16,7 +16,7 @@ let handler = async (m, { conn, participants, groupMetadata, args }) => {
 └───────────
 
 *ADMINISTRADORES:*
-${listAdmin}
+⚠️ ${listAdmin}
 `
 
     text = text.trim()
