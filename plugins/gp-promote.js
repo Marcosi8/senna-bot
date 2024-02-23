8e4b81871826b0b2021a6fd108210bb167e00fee
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-23 19:20:53 -0300

    Update gp-promote.js

--- plugins/gp-promote.js
+++ plugins/gp-promote.js
@@ -21,9 +21,9 @@ var user = number + '@s.whatsapp.net'
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
 m.reply(`✅ ${mssg.promote}`)
 }}
-handler.help = ['promote']
+handler.help = ['promover']
 handler.tags = ['group']
-handler.command = ['promote', 'promover'] 
+handler.command = ['promote', 'promover', 'promove'] 
 handler.group = true
 handler.admin = true
 handler.botAdmin = true
