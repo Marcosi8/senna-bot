98769d624754521440138a13444b2e5fe16eef9f
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 22:54:20 -0300

    Update gp‐chamaram.js

--- plugins/gp‐chamaram.js
+++ plugins/gp‐chamaram.js
@@ -1,15 +1,15 @@
 let handler = async (message, { conn, participants, groupMetadata, args, usedPrefix, text, command }) => {
   if (!text) return message.reply(`Por favor, inclua uma mensagem para enviar para todos os moderadores do grupo.`)
-  const pp = groupMetadata?.profilePicThumbObj?.eurl || './src/avatar_contact.png'
-  const groupModerators = participants.filter(p => p.isAdmin)
-  const listModerators = groupModerators.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
-  const owner = groupMetadata.owner || groupModerators.find(p => p.isAdmin === 'superadmin')?.id || message.chat.split`-`[0] + '@s.whatsapp.net'
+  const pp = groupMetadata?.profilePicThumbObj?.eurl || './src/admins.jpg'
+  const groupAdmins = participants.filter(p => p.isAdmin)
+  const listAdmins = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.jid.split('@')[0]}*`).join('\n')
+  const owner = groupMetadata.owner || groupAdmins.find(p => p.isAdmin === 'superadmin')?.jid || message.chat.split`-`[0] + '@s.whatsapp.net'
   let pesan = args.join` `
   let formattedMessage = `*Mensagem do Bot:*\n${pesan}`
-  let moderatorListMessage = `*Lista de Moderadores:*\n${listModerators}`
+  let adminsListMessage = `*Lista de Administradores:*\n${listAdmins}`
   
   try {
-    await conn.sendFile(message.chat, pp, 'moderators_message.jpg', Buffer.from(formattedMessage + '\n\n' + moderatorListMessage), message, false, { mentions: [...groupModerators.map(v => v.id), owner] })
+    await conn.sendFile(message.chat, pp, 'moderators_message.jpg', Buffer.from(formattedMessage + '\n\n' + adminsListMessage), message, false, { mentions: [...groupAdmins.map(v => v.jid), owner] })
   } catch (error) {
     console.error(error)
     message.reply('Ocorreu um erro ao enviar a mensagem para os moderadores do grupo.')
