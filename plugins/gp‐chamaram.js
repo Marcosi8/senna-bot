68767cd8c91c865dfdbab5da16b34053089758cb
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 23:03:46 -0300

    Update gp‐chamaram.js

--- plugins/gp‐chamaram.js
+++ plugins/gp‐chamaram.js
@@ -1,24 +1,22 @@
-let handler = async (message, { conn, participants, groupMetadata, args, usedPrefix, text, command }) => {
-  if (!text) return message.reply(`Por favor, inclua uma mensagem para enviar para todos os moderadores do grupo.`)
+let handler = async (message, { conn, participants, groupMetadata, args }) => {
   const pp = groupMetadata?.profilePicThumbObj?.eurl || './src/avatar_contact.png'
   const groupAdmins = participants.filter(p => p.isAdmin)
-  const listAdmins = groupAdmins.map((v, i) => `*» ${i + 1}. @${v.jid.split('@')[0]}*`).join('\n')
-  const owner = groupMetadata.owner || groupAdmins.find(p => p.isAdmin === 'superadmin')?.jid || message.chat.split`-`[0] + '@s.whatsapp.net'
-  let pesan = args.join` `
-  let formattedMessage = `*Mensagem do Bot:*\n${pesan}`
-  let adminsListMessage = `*Lista de Administradores:*\n${listAdmins}`
+  const listAdmins = groupAdmins.map((v, i) => `${i + 1}. @${v.jid.split('@')[0]}`).join('\n▢ ')
+  const owner = groupMetadata.owner || groupAdmins.find(p => p.isAdmin === 'superadmin') || message.chat.split`-`[0] + '@s.whatsapp.net'
+  
+  let messageText = `*Administradores do Grupo:*\n${listAdmins}`
   
   try {
-    await conn.sendFile(message.chat, pp, 'moderators_message.jpg', Buffer.from(formattedMessage + '\n\n' + adminsListMessage), message, false, { mentions: [...groupAdmins.map(v => v.jid), owner] })
+    await conn.sendFile(message.chat, pp, 'admins_message.jpg', Buffer.from(messageText), message, false, { mentions: groupAdmins.map(v => v.jid) })
   } catch (error) {
     console.error(error)
-    message.reply('Ocorreu um erro ao enviar a mensagem para os moderadores do grupo.')
+    message.reply('Ocorreu um erro ao chamar os administradores do grupo.')
   }
 }
 
 handler.tags = ['prime'] // Tags para identificar o comando
-handler.help = ['moderadores', '@moderadores', 'mods'] // Comandos de ajuda
+handler.help = ['admins', '@admins', 'dmins'] // Comandos de ajuda
 
-handler.command = /^(moderadores|@moderadores|mods)$/i
+handler.command = /^(admins|@admins|dmins)$/i
 handler.group = true
 export default handler
