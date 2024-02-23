4d23df5f5768efda0be4dd27beb7fb6cf49e3b27
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-23 17:32:34 -0300

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -7,14 +7,14 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
     let chat = global.db.data.chats[m.chat];
     let bot = global.db.data.settings[this.user.jid] || {};
     const isGroupLink = linkRegex.test(m.text); // Alterado para testar a presença de links
-    const listAdmin = Object.keys(chat.admins || {}).map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n❗️'); // Corrigido para acessar chat.admins corretamente
+    const listAdmin = Object.keys(chat.admins || {}).map((v, i) => `${i + 1}. @${v.split('@')[0]}`).join('\n');
 
     if (chat.antiLink && isGroupLink && !isAdmin) {
         if (isBotAdmin) {
             const linkThisGroup = `https://chat.whatsapp.com/${await this.groupInviteCode(m.chat)}`;
             if (m.text.includes(linkThisGroup)) return !0;
         }
-        const banMessage = `> *[❗️] LINK DETECTED 🔗*\n\n*We do not allow unknown links in our group*\n\nNão permitimos links desconhecidos em nosso grupo, @${m.sender.split('@')[0]}. O banimento é automático, contate um administrador se acha que foi um erro. ${isBotAdmin ? '' : '\n\n⚠️ Eu não sou um administrador, então eu não posso expulsá-lo. Chamada por administradores do grupo:\n\n${listAdmin}'}`;
+        const banMessage = `> *[❗️] LINK DETECTED 🔗*\n\n*We do not allow unknown links in our group*\n\nNão permitimos links desconhecidos em nosso grupo, @${m.sender.split('@')[0]}. O banimento é automático, contate um administrador se acha que foi um erro. ${isBotAdmin ? '' : `\n\n⚠️ Eu não sou um administrador, então eu não posso expulsá-lo. Chamada por administradores do grupo:\n\n${listAdmin}`}`;
         await conn.reply(m.chat, banMessage, null, { mentions: [m.sender] });
         if (isBotAdmin && chat.antiLink) {
             await conn.sendMessage(m.chat, { delete: m.key });
