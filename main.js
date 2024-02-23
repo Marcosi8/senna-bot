64b87d200ee3608ce4915a462d19e2c463163b15
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-23 14:33:28 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
    
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
+console.log('🤖 Bot iniciado com sucesso!');
 import './config.js'; 
 import { createRequire } from "module"; // Bring in the ability to create the 'require' method
 import path, { join } from 'path'
