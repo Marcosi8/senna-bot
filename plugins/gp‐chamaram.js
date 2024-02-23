f98c85666317dd6676637293b414118e80056b2b
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-22 22:48:41 -0300

    Update gp‐chamaram.js

--- plugins/gp‐chamaram.js
+++ plugins/gp‐chamaram.js
@@ -1,13 +1,19 @@
 let handler = async (message, { conn, participants, groupMetadata, args, usedPrefix, text, command }) => {
   if (!text) return message.reply(`Por favor, inclua uma mensagem para enviar para todos os moderadores do grupo.`)
-  const pp = await conn.profilePictureUrl(message.chat, 'image').catch(_ => null) || './src/admins.jpg'
+  const pp = await conn.getProfilePicture(message.chat).catch(_ => null)
   const groupModerators = participants.filter(p => p.isAdmin)
   const listModerators = groupModerators.map((v, i) => `*» ${i + 1}. @${v.id.split('@')[0]}*`).join('\n')
   const owner = groupMetadata.owner || groupModerators.find(p => p.isAdmin === 'superadmin')?.id || message.chat.split`-`[0] + '@s.whatsapp.net'
   let pesan = args.join` `
   let formattedMessage = `*Mensagem do Bot:*\n${pesan}`
   let moderatorListMessage = `*Lista de Moderadores:*\n${listModerators}`
-  await conn.sendFile(message.chat, pp, 'moderators_message.jpg', formattedMessage + '\n\n' + moderatorListMessage, message, false, { mentions: [...groupModerators.map(v => v.id), owner] })
+  
+  try {
+    await conn.sendFile(message.chat, pp, 'moderators_message.jpg', Buffer.from(formattedMessage + '\n\n' + moderatorListMessage), message, false, { mentions: [...groupModerators.map(v => v.id), owner] })
+  } catch (error) {
+    console.error(error)
+    message.reply('Ocorreu um erro ao enviar a mensagem para os moderadores do grupo.')
+  }
 }
 
 handler.tags = ['prime'] // Tags para identificar o comando
