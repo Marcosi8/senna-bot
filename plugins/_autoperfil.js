54a0bb583c6de768186886242d244fad6132dc3f
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-24 17:57:46 -0300

    Update _autoperfil.js

--- plugins/_autoperfil.js
+++ plugins/_autoperfil.js
@@ -1,15 +1,20 @@
 import fs from 'fs';
 
 let handler = async (m, { conn }) => {
+    console.log('Verificando atualização da foto de perfil...');
+    
     let setting = global.db.data.settings[this.user.jid];
     let currentTime = new Date().getTime();
     let lastUpdate = setting.lastUpdate || 0;
     let updateInterval = 24 * 60 * 60 * 1000; // 24 horas em milissegundos
     
     if (currentTime - lastUpdate >= updateInterval || lastUpdate === 0) {
+        console.log('Atualizando a foto de perfil...');
         await updateProfilePhoto(conn);
         setting.lastUpdate = currentTime;
         global.db.save();
+    } else {
+        console.log('A foto de perfil não precisa ser atualizada neste momento.');
     }
 };
 
@@ -24,6 +29,7 @@ async function updateProfilePhoto(conn) {
     try {
         let imgBuffer = fs.readFileSync('./src/GPT/gpt1.jpeg'); // Substitua pelo caminho da sua imagem
         await conn.updateProfilePicture(conn.user.jid, imgBuffer);
+        console.log('Foto de perfil atualizada com sucesso.');
     } catch (error) {
         console.error('Erro ao atualizar a foto de perfil:', error);
     }
