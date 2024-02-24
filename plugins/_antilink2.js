5e2e97b11765e112803ac03ca67dae0365724184
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-24 14:19:49 -0300

    Update _antilink2.js

--- plugins/_antilink2.js
+++ plugins/_antilink2.js
@@ -10,11 +10,14 @@ export async function before(m, { conn, isAdmin, isBotAdmin }) {
 
     if (m.text.startsWith('antilink2') && isAdmin) {
         chat.antiLink2 = !chat.antiLink2;
-        conn.reply(m.chat, `Antilink 2 (apenas apaga mensagens) agora está ${chat.antiLink2 ? 'ativado' : 'desativado'}.`, m)
+        conn.reply(m.chat, `Anti-link 2 agora está ${chat.antiLink2 ? 'ativado' : 'desativado'}.`, m)
         return !0;
     }
 
     if (chat.antiLink2 && isLink && !isAdmin) {
+        await conn.reply(m.chat, `> *[❗️] LINK DETECTED 🔗*
+            
+*We do not allow unknown links in our group.*\n\n_Não permitimos links desconhecidos em nosso grupo,_ @${m.sender.split('@')[0]}. _Antilink2 ativo, não é passível de banimento._ ${isBotAdmin ? '' : '\n\n⚠️ *Eu não sou um administrador do grupo, então eu não posso excluir o link!*'}`, null, { mentions: [m.sender] });
         await conn.sendMessage(m.chat, { delete: m.key });
         return !1;
     }
