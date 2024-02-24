260d63331c17245de264da3caa3dbb04bedc383a
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-23 21:11:22 -0300

    Update loop.js

--- plugins/loop.js
+++ plugins/loop.js
@@ -1,16 +1,17 @@
-let typingLoop = async (conn) => {
+const typingLoop = async (conn, chatId) => {
     while (true) {
-        await conn.chatUpdate(conn.user.jid, 'typing')
+        await conn.updatePresence(chatId, Presence.composing)
         await new Promise(resolve => setTimeout(resolve, 5000)) // Aguarda 5 segundos antes de enviar novamente
     }
 }
 
-const handler = async ({ conn }) => {
-    typingLoop(conn)
+const handler = async ({ conn, args }) => {
+    const chatId = args[0] + '@s.whatsapp.net' // Obtendo o ID do chat a partir dos argumentos
+    typingLoop(conn, chatId).catch(err => console.error(err)) // Adicionando tratamento de erro
 }
 
-handler.help = ['typingloop']
-handler.tags = ['prime']
+handler.help = ['typingloop <chatId>']
+handler.tags = ['tools']
 handler.command = ['typingloop']
 
 export default handler
