4bd571c9b3954059342717752ec0597069a519a0
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-24 18:59:41 -0300

    Update _autoperfil.js

--- plugins/_autoperfil.js
+++ plugins/_autoperfil.js
@@ -2,39 +2,14 @@ import fs from 'fs';
 
 let handler = m => m;
 handler.all = async function (m, { conn }) {
-    if (!conn) return;
-
-    let setting = global.db.data.settings[this.user.jid];
-
-    let _uptime = process.uptime() * 1000;
-    let _muptime;
-    if (process.send) {
-        process.send('uptime');
-        _muptime = await new Promise(resolve => {
-            process.once('message', resolve);
-            setTimeout(resolve, 2000);
-        }) * 1000;
-    }
-    let uptime = clockString(_uptime);
-    
     try {
         let imgBuffer = fs.readFileSync('./src/GPT/gpt1.jpeg');
-        await conn.updateProfilePicture(conn.user.me.user);
+        await conn.updateProfilePicture(conn.user.jid, imgBuffer);
         console.log('Foto de perfil atualizada com sucesso.');
     } catch (error) {
         console.error('Erro ao atualizar a foto de perfil:', error);
     }
-    
-    setting.status = new Date() * 1;
     return m;
 };
 
 export default handler;
-
-function clockString(ms) {
-    let d = isNaN(ms) ? '--' : Math.floor(ms / 86400000);
-    let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000) % 24;
-    let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60;
-    let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60;
-    return [d, ' » ', h, ' ・ ', m, ' ・ ', s].map(v => v.toString().padStart(2, 0)).join('');
-}
