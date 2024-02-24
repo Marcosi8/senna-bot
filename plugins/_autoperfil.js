48c2bc0def56941b4df6ba75a8f82b6849c7ebb4
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-24 18:10:59 -0300

    Update _autoperfil.js

--- plugins/_autoperfil.js
+++ plugins/_autoperfil.js
@@ -1,33 +1,22 @@
 import fs from 'fs';
 
 let handler = async ({ conn }) => {
-    console.log('Verificando atualização da foto de perfil...');
-    
     let setting = global.db.data.settings;
     let currentTime = new Date().getTime();
-    let lastUpdate = setting.lastUpdate || 0;
+    let lastUpdate = setting.lastProfileUpdate || 0;
     let updateInterval = 24 * 60 * 60 * 1000; // 24 horas em milissegundos
     
-    console.log('Última atualização:', new Date(lastUpdate).toLocaleString());
-    
     if (currentTime - lastUpdate >= updateInterval || lastUpdate === 0) {
-        console.log('Atualizando a foto de perfil...');
-        await updateProfilePhoto(conn);
-        setting.lastUpdate = currentTime;
-        global.db.save();
-    } else {
-        console.log('A foto de perfil não precisa ser atualizada neste momento.');
+        try {
+            let imgBuffer = fs.readFileSync('./src/GPT/gpt1.jpeg');
+            await conn.updateProfilePicture(conn.user.jid, imgBuffer);
+            setting.lastProfileUpdate = currentTime;
+            global.db.save();
+            console.log('Foto de perfil atualizada com sucesso.');
+        } catch (error) {
+            console.error('Erro ao atualizar a foto de perfil:', error);
+        }
     }
 };
 
 export default handler;
-
-async function updateProfilePhoto(conn) {
-    try {
-        let imgBuffer = fs.readFileSync('./src/GPT/gpt1.jpeg');
-        await conn.updateProfilePicture(conn.user.jid, imgBuffer);
-        console.log('Foto de perfil atualizada com sucesso.');
-    } catch (error) {
-        console.error('Erro ao atualizar a foto de perfil:', error);
-    }
-}
