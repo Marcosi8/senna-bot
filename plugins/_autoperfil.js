d78650c8fe12d7e870ab661470738d91d20a3b1e
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-24 18:00:53 -0300

    Update _autoperfil.js

--- plugins/_autoperfil.js
+++ plugins/_autoperfil.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
 
-let handler = async (m, { conn }) => {
+let handler = async ({ conn }) => {
     console.log('Verificando atualização da foto de perfil...');
     
-    let setting = global.db.data.settings[this.user.jid];
+    let setting = global.db.data.settings;
     let currentTime = new Date().getTime();
     let lastUpdate = setting.lastUpdate || 0;
     let updateInterval = 24 * 60 * 60 * 1000; // 24 horas em milissegundos
@@ -16,11 +16,6 @@ let handler = async (m, { conn }) => {
     } else {
         console.log('A foto de perfil não precisa ser atualizada neste momento.');
     }
-};
-
-handler.all = async (m, { conn }) => {
-    handler(m, { conn });
-    return m;
 };
 
 export default handler;
