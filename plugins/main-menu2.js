92dfca5bfdf94c4fdeb51ba7c3dd7c77fc1357c4
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-25 15:32:55 -0300

    Update main-menu2.js

--- plugins/main-menu2.js
+++ plugins/main-menu2.js
@@ -2,33 +2,34 @@
 let handler = async function (m, { conn, text, usedPrefix }) {
   
 let m2 = `
-> Lista de comandos
+> Lista de Opções
 
 > *ADMIN*
 
-captcha
-welcome
-antilink (com kick user)
-antilink2 (sem kick user)
-detect 
-document
-nsfw
-autosticker
+ ${usedPrefix}captcha
+ ${usedPrefix}welcome/bemvindo
+ ${usedPrefix}setbye
+ ${usedPrefix}antilink (com kick user)
+ ${usedPrefix}antilink2 (sem kick user)
+ ${usedPrefix}detect 
+ ${usedPrefix}document
+ ${usedPrefix}nsfw
+ ${usedPrefix}autosticker
 └───────────── 
 > *USERS*
 
-autolevelup
-chatbot 
+ ${usedPrefix}autolevelup
+ ${usedPrefix}chatbot 
 └─────────────
 > *OWNER*
 
-public
-sopv
-sogp
+ ${usedPrefix}public
+ ${usedPrefix}sopv
+ ${usedPrefix}sogp
 └─────────────
 *📌 Exemplo:*
-*${usedPrefix}on* welcome
-*${usedPrefix}off* welcome
+*${usedPrefix}on* antilink2
+*${usedPrefix}off* antilink2
 `
     let pp = './src/WhatsApp Video 2024-02-08 at 16.59.44.mp4' 
     /*conn.sendButton(m.chat, m2, mssg.ig, pp, [
