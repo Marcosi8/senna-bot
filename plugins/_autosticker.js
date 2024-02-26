9f1753b66960d7ec654df1cede8ee513f357917e
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-26 20:25:35 -0300

    Update _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -26,7 +26,7 @@ if (isUrl(m.text)) stiker = await sticker(false, m.text.split(/\n| /i)[0], packn
 else return
 }
 if (stiker) {
-await conn.sendFile(m.chat, stiker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: gt, body: ' *SoyuzBOT* ',  mediaType: 2, thumbnail: marcosgpt.getRandom(), sourceUrl: marcosMenu.getRandom()}}}, { quoted: m })
+await conn.sendFile(m.chat, sticker, 'sticker.webp', '',m, true, { contextInfo: { 'forwardingScore': 200, 'isForwarded': false, externalAdReply:{ showAdAttribution: false, title: gt, body: ' *SoyuzBOT* ',  mediaType: 2, thumbnail: marcosgpt.getRandom(), sourceUrl: marcosMenu.getRandom()}}}, { quoted: m })
 //this.sendFile(m.chat, stiker, null, { asSticker: true })
 }}
 return !0
