5d92c0180a5b958aee2dd0d6493bb432f1ba894e
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-27 09:12:09 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,12 +4,12 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url'
 
 global.owner = [
-  ['558881647724', 'marcoskz_', true],
-  ['558881647724']
+  ['61488873561', 'marcoskz_', true],
+  ['']
 ] //Numeros dos donos
 
-global.mods = ['558881647724'] 
-global.prems = ['558881647724', '558881647724']
+global.mods = [''] 
+global.prems = ['', '']
 global.APIs = { // API Prefix
   // name: 'https://website' 
   nrtm: 'https://fg-nrtm.ddns.net',
