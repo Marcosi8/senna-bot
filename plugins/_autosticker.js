a47404353a9ee4673d3473d7e5da0fe902b832e2
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-27 20:03:25 -0300

    Update _autosticker.js

--- plugins/_autosticker.js
+++ plugins/_autosticker.js
@@ -31,7 +31,7 @@ await conn.sendFile(m.chat, sticker, 'sticker.webp', '',m, true, { contextInfo: 
 }}
 return !0
 }
-export default handler
+export default handler/*
 
 const isUrl = (text) => {
-return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))}/*
+return text.match(new RegExp(/^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))}
