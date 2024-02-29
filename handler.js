a05746dd32abac6d86b013bd643219cb3bde20fb
Marcos Oliveira <76185975+Marcosi8@users.noreply.github.com>
2024-02-29 16:07:00 -0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -545,7 +545,7 @@ export async function participantsUpdate({ id, participants, action }) {
             } finally {
                 const botTt2 = groupMetadata.participants.find(u => this.decodeJid(u.id) == this.user.jid) || {};
                 const isBotAdminNn = botTt2?.admin === "admin" || false;
-                const welcomeMessage = chat.sWelcome || this.welcome || conn.welcome || `Bem-vindo, ao grupo @user! leia nossa @desc`;
+                const welcomeMessage = chat.sWelcome || this.welcome || conn.welcome || `Bem-vindo ao grupo @user! leia nossa @desc`;
                 const byeMessage = chat.sBye || this.bye || conn.bye || `Adeus, @user`;
                 const messageContent = action === 'add' ? welcomeMessage.replace('@user', `@${user.split('@')[0]}`).replace('@desc', groupMetadata.desc?.toString() || 'Desconocido').replace('@grupo', await this.getName(id)) : byeMessage.replace('@user', `@${user.split('@')[0]}`).replace('@desc', groupMetadata.desc?.toString() || 'Desconhecido').replace('@grupo', await this.getName(id));
                 //const messageContent = action === 'add' ? welcomeMessage.replace('@user', `@${user.split('@')[0]}`).replace('@desc', groupMetadata.desc?.toString() || 'Desconocido').replace('@grupo', await this.getName(id)) : byeMessage.replace('@subject', `@${await this.getName(id)}`).replace('@desc', groupMetadata.desc?.toString() || 'Desconhecido').replace('@group', await this.getName(id));
